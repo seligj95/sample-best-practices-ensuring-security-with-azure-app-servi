b1dccdda372640c1428a8903b003b3bbd5fa1037
Jordan Selig <jordanselig@microsoft.com>
2026-03-21 21:50:28 -0400

    add src/index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, secure world! Your app is signed and deployed securely.');
+  res.send('Hello, Secure World! Your app is signed and deployed securely.');
 });
 
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on http://localhost:${port}`);
 });
