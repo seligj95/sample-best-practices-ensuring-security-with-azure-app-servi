b1797974d934c6c9d240392fbfa004e4fcd09b55
Jordan Selig <jordanselig@microsoft.com>
2026-03-21 21:48:22 -0400

    add src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,9 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, Azure App Service! Your app is securely signed.');
+  res.send('Hello, Azure App Service with secure code signing!');
 });
 
 app.listen(port, () => {
-  console.log(`App running on http://localhost:${port}`);
+  console.log(`App listening on port ${port}`);
 });
