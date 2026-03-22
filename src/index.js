4f8d1ee1c1c8a5fd8b18655026fe1647713cd3a0
Jordan Selig <jordanselig@microsoft.com>
2026-03-21 21:45:16 -0400

    add src/index.js

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello, Azure App Service! Your app is securely signed.');
+});
+
+app.listen(port, () => {
+  console.log(`App running at http://localhost:${port}`);
+});
